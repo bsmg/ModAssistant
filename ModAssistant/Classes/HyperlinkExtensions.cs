fe158c70726d2c3d6a23bacfcc8a709f4c49525a
Jack Baron <jackmbaron@gmail.com>
2020-02-02 02:41:31 +0000

    extension method for handling external links

---
+++ ModAssistant/Classes/HyperlinkExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace ModAssistant
+{
+    public static class HyperlinkExtensions
+    {
+        public static bool GetIsExternal(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsExternalProperty);
+        }
+
+        public static void SetIsExternal(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsExternalProperty, value);
+        }
+
+        public static readonly DependencyProperty IsExternalProperty = DependencyProperty.RegisterAttached("IsExternal", typeof(bool), typeof(HyperlinkExtensions), new UIPropertyMetadata(false, OnIsExternalChanged));
+
+        private static void OnIsExternalChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            var hyperlink = sender as Hyperlink;
+
+            if ((bool)args.NewValue)
+                hyperlink.RequestNavigate += Hyperlink_RequestNavigate;
+            else
+                hyperlink.RequestNavigate -= Hyperlink_RequestNavigate;
+        }
+
+        private static void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
