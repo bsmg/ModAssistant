3645f88edac0946cd867ff8678bf1819341358f9
Parapass <parapasser@gmail.com>
2020-04-05 11:01:21 -0400

    revert experimental change

--- ModAssistant/Classes/OneClickInstaller.cs
+++ ModAssistant/Classes/OneClickInstaller.cs
@@ -144,13 +144,8 @@ namespace ModAssistant
                     await DownloadAsset(BSaberURLPrefix + filename, CustomPlaylistsFolder);
                     Playlist playlist = JsonSerializer.Deserialize<Playlist>(File.ReadAllText(Path.Combine(BeatSaberPath, CustomPlaylistsFolder, filename)));
                     foreach (Playlist.Song song in playlist.songs)
-                    try
                     {
                         await BeatSaver(new Uri("beatsaver://" + song.key));
-                    }
-                    catch (Exception e)
-                    {
-                        Application.Current.FindResource("OneClick:SongDownload:Failed");
                     }
                     break;
             }
