172696f4274edd8f77d1c37d244f4fc1f659e8cb
Parapass <parapasser@gmail.com>
2020-04-10 12:28:10 -0400

    do half complete pr thing

--- ModAssistant/Classes/OneClickInstaller.cs
+++ ModAssistant/Classes/OneClickInstaller.cs
@@ -149,7 +149,7 @@ namespace ModAssistant
 
                         if (string.IsNullOrEmpty(song.key))
                         {
-                            await BeatSaver(new Uri("beatsaver://" + song.hash));
+                            await BeatSaver(new Uri(BeatSaverURLPrefix + "api/download/hash/" + song.hash));
                         }
                     }
                     break;
@@ -364,6 +364,44 @@ namespace ModAssistant
         {
             public string _id { get; set; }
             public string username { get; set; }
+        }
+    }
+    class BeatSaverHashResponse
+    {
+        public Metadata metadata { get; set; }
+        public Stats stats { get; set; }
+        public string description { get; set; }
+        public DateTime? deletedAt { get; set; }
+        public string _id { get; set; }
+        public string key { get; set; }
+        public string name { get; set; }
+        public Uploader uploader { get; set; }
+        public string hash { get; set; }
+        public string directDownload { get; set; }
+        public string downloadURL { get; set; }
+        public string coverURL { get; set; }
+        public class Metadata
+        {
+            public Difficulties difficulties { get; set; }
+            public Characteristic[] characteristics { get; set; }
+            public string songName { get; set; }
+            public string songSubName { get; set; }
+            public string songAuthorName { get; set; }
+            public string levelAuthorName { get; set; }
+            public double bpm { get; set; }
+        }
+        public class Difficulties
+        {
+            public bool easy { get; set; }
+            public bool normal { get; set; }
+            public bool hard { get; set; }
+            public bool expert { get; set; }
+            public bool expertPlus { get; set; }
+        }
+        public class Characteristic
+        {
+            public Difficulties difficulties { get; set; }
+            public string name { get; set; }
         }
     }
 }
