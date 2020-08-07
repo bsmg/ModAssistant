aa59313d512d3eeaeeffad88786e7f579266e083
WGzeyu <785777793@qq.com>
2020-08-07 23:10:46 +0800

    Updated version to 1.1.1318

--- ModAssistant/Properties/AssemblyInfo.cs
+++ ModAssistant/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1317.0")]
-[assembly: AssemblyFileVersion("1.1.1317.0")]
+[assembly: AssemblyVersion("1.1.1318.0")]
+[assembly: AssemblyFileVersion("1.1.1318.0")]
