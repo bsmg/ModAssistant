6d7e02aaa4b939b9afef8c87fe83062a7fd079ca
Eris <erisapps@outlook.be>
2021-03-15 23:01:54 +0100

    Fixed inconsistent naming ModAssistant vs Mod Assistant
    Melopod ƸӜƷ — Today at 22:42
    @Assistant hey, would you prefer it being ModAssistant or Mod Assistant
    Space or no space
    
    Assistant — Today at 22:43
    Mod Assistant

--- ModAssistant/Properties/AssemblyInfo.cs
+++ ModAssistant/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModAssistant")]
+[assembly: AssemblyTitle("Mod Assistant")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModAssistant")]
+[assembly: AssemblyProduct("Mod Assistant")]
 [assembly: AssemblyCopyright("Copyright © Assistant 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
