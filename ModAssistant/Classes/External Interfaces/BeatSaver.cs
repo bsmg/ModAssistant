ec4ed6f1edad6dd23219034751b9434fca248c49
HiImMilan <77924967+HiImMilan@users.noreply.github.com>
2021-12-23 16:37:35 +0100

    Notification Fix

--- ModAssistant/Classes/External Interfaces/BeatSaver.cs
+++ ModAssistant/Classes/External Interfaces/BeatSaver.cs
@@ -27,7 +27,6 @@ namespace ModAssistant.API
 
         public static async Task<BeatSaverMap> GetFromHash(string Hash, bool showNotification = true)
         {
-            MessageBox.Show("hash," + App.OCIWindow + "," + showNotification);
             if (showNotification && App.OCIWindow != "No" && App.OCIWindow != "Notify") OneClickInstaller.Status.Show();
             return await GetMap(Hash, "hash", showNotification);
         }
