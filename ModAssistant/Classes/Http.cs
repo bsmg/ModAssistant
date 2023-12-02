b88167e63e3be0a9c2b98ecf0c84fadb19110cc1
baoziii <18572746+baoziii@users.noreply.github.com>
2023-12-02 15:40:34 -0500

    Add Brotli Support

--- ModAssistant/Classes/Http.cs
+++ ModAssistant/Classes/Http.cs
@@ -1,7 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Threading;
 using System.Web.Script.Serialization;
+using BrotliSharpLib;
+using System.Linq;
+using System.IO.Compression;
+using System.IO;
 
 namespace ModAssistant
 {
@@ -15,17 +23,17 @@ namespace ModAssistant
             {
                 if (_client != null) return _client;
 
-                var handler = new HttpClientHandler()
+                /*var handler = new HttpClientHandler()
                 {
                     AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-                };
+                };*/
 
-                _client = new HttpClient(handler)
+                _client = new HttpClient(new BrotliCompressionHandler())
                 {
                     Timeout = TimeSpan.FromSeconds(360),
                 };
 
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.SystemDefault;
                 _client.DefaultRequestHeaders.Add("User-Agent", "ModAssistant/" + App.Version);
 
                 return _client;
@@ -36,5 +44,66 @@ namespace ModAssistant
         {
             MaxJsonLength = int.MaxValue,
         };
+
+        private class BrotliCompressionHandler : DelegatingHandler
+        {
+            public BrotliCompressionHandler() : base(new HttpClientHandler())
+            {
+            }
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                // Specify supported encodings in the request
+                request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("br"));
+                request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+                request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+
+                return base.SendAsync(request, cancellationToken).ContinueWith(responseTask =>
+                {
+                    HttpResponseMessage response = responseTask.Result;
+
+                    // Check if the response is compressed
+                    if (response.Content.Headers.ContentEncoding != null)
+                    {
+                        foreach (var encoding in response.Content.Headers.ContentEncoding)
+                        {
+                            switch (encoding.ToLowerInvariant())
+                            {
+                                case "br":
+                                    // Decompress Brotli content
+                                    byte[] brotliCompressedBytes = response.Content.ReadAsByteArrayAsync().Result;
+                                    byte[] brotliDecompressedBytes = Brotli.DecompressBuffer(brotliCompressedBytes, 0, brotliCompressedBytes.Length);
+                                    response.Content = new ByteArrayContent(brotliDecompressedBytes);
+                                    response.Content.Headers.Remove("Content-Encoding");
+                                    break;
+
+                                case "gzip":
+                                    // Decompress GZip content
+                                    using (var stream = new GZipStream(response.Content.ReadAsStreamAsync().Result, CompressionMode.Decompress))
+                                    {
+                                        var resultStream = new MemoryStream();
+                                        stream.CopyTo(resultStream);
+                                        response.Content = new ByteArrayContent(resultStream.ToArray());
+                                    }
+                                    response.Content.Headers.Remove("Content-Encoding");
+                                    break;
+
+                                case "deflate":
+                                    // Decompress Deflate content
+                                    using (var stream = new DeflateStream(response.Content.ReadAsStreamAsync().Result, CompressionMode.Decompress))
+                                    {
+                                        var resultStream = new MemoryStream();
+                                        stream.CopyTo(resultStream);
+                                        response.Content = new ByteArrayContent(resultStream.ToArray());
+                                    }
+                                    response.Content.Headers.Remove("Content-Encoding");
+                                    break;
+                            }
+                        }
+                    }
+
+                    return response;
+                }, cancellationToken);
+            }
+        }
     }
 }
